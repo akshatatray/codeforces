2e16d1c8a3d18d1a8a9938a29a044e78526df4a6
Akshat Atray <akshatatray31@gmail.com>
2021-05-24 09:37:19 +0530

    479

---
+++ 479-Expression.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a, b, c, res=INT_MIN;
+    cin>>a>>b>>c;
+    res > a+b+c ?  : res = a+b+c;
+    res > a*b*c ?  : res = a*b*c;
+    res > a+(b*c) ?  : res = a+(b*c);
+    res > (a+b)*c ?  : res = (a+b)*c;
+    res > (a*b)+c ?  : res = (a*b)+c;
+    res > a*(b+c) ?  : res = a*(b+c);
+    cout<<res<<endl;
+    return 0;
+}
